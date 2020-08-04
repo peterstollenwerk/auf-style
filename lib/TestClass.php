32d1c5f45bf930c3665840299720976a48c57e25
PeterStollenwerk <ps@peterstollenwerk.de>
2020-08-04 10:31:03 +0200

    first commit

---
+++ lib/TestClass.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace auf;
+
+class TestClass {
+
+  public $test;
+  
+  public function __construct(string $value) {
+    
+    $this->test = $value;
+
+  }
+
+}
---
+++ tests/suites/MyTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace auf;
+
+use PHPUnit\Framework\TestCase;
+use auf\TestClass;
+
+final class PluginTest extends TestCase {
+  public function testTitleValue () {
+    $this->assertEquals(site()->title()->value(), 'Auf-Style-Plugin');
+  }
+
+  public function testCreatesExampleClass () {
+    $myTestClass = new TestClass('asdf');
+    $this->assertEquals('asdf', $myTestClass->test);
+  }
+
+}
