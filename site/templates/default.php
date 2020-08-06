bbec423e760c04099ff69ed642a5d5d30c8f7661
PeterStollenwerk <ps@peterstollenwerk.de>
2020-08-06 12:16:33 +0200

    wip color-themes snippet

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,32 +1,45 @@
 <h1><?= $page->title() ?></h1>
 
 <?php
-
+  $wrapTag = 'pre';
   $colorThemes = $site->color_themes()->toStructure();
-
 ?>
 
-<?php foreach($colorThemes as $theme): ?>
+<?php if($wrapTag): ?>
+  <<?= $wrapTag ?>>
+<?php endif?>
+
+<?php foreach($colorThemes as $colorTheme): ?>
 
   <?php 
-      $uid = $theme->uid();
+      $uid = $colorTheme->uid();
       $css_class = '.color-theme--'. $uid;
-
-      $color = $theme->color();
+      # colors
+      $color = $colorTheme->color();
+      $backgroundColor = $colorTheme->background();
+      $borderColor = $colorTheme->border();
+      $outlineColor = $colorTheme->outline();
+      $textShadowColor = $colorTheme->text_shadow();
+      $boxShadowColor = $colorTheme->box_shadow();
   ?>
   
-  <pre>
     <?= $css_class ?> {
+      <?php if($color->isNotEmpty()): ?>--color: <?= $color ?>;<?php endif?>
+
+      <?php if($backgroundColor->isNotEmpty()): ?>--background-color: <?= $backgroundColor ?>;<?php endif?>
+        
+      <?php if($borderColor->isNotEmpty()): ?>--border-color: <?= $borderColor ?>;<?php endif?>
       
-    <?php if($color->isNotEmpty()): ?>
-    --color: <?= $color->toColor('hsl') ?>;
-    <?php endif?>
+      <?php if($outlineColor->isNotEmpty()): ?>--outline-color: <?= $outlineColor ?>;<?php endif?>
+      
+      <?php if($textShadowColor->isNotEmpty()): ?>--text-shadow-color: <?= $textShadowColor ?>;<?php endif?>
+
+      <?php if($boxShadowColor->isNotEmpty()): ?>--box-shadow-color: <?= $boxShadowColor ?>;<?php endif?>
 
     }
-  </pre>
 
 <?php endforeach?>
 
-
-377
-12:45
+<?php if($wrapTag): ?>
+  </<?= $wrapTag ?>>
+<?php endif?>
