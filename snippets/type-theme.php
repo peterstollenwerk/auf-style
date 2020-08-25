d98bcef0d927d1dfacde32f40e90b116df3be7b1
PeterStollenwerk <ps@peterstollenwerk.de>
2020-08-25 18:32:04 +0200

    implemented type themes

--- snippets/type-theme.php
+++ snippets/type-theme.php
@@ -13,7 +13,7 @@ $renderCssClass = isset($renderCssClass) ? $renderCssClass : true;
   if($cssSelector->isNotEmpty()) array_push($cssClasses, $cssSelector);
   $cssClass = implode(', ', $cssClasses);
 
-  $fontSize = (string )$typeTheme->font_size();
+  $fontSize = (string) $typeTheme->font_size();
 
   $fontSizesMap = [
     'font-size--7'  => 'var(--size-7)',
@@ -29,6 +29,22 @@ $renderCssClass = isset($renderCssClass) ? $renderCssClass : true;
     'font-size---3' => 'var(--size--3)',
     'font-size---4' => 'var(--size--4)',
   ];
+
+  $fontWeight = (string) $typeTheme->font_weight();
+
+  $fontWeightMap = [
+    'font-weight--normal' => 'normal',
+    'font-weight--bold' => 'bold',
+  ];
+  
+  $fontStyle = (string) $typeTheme->font_style();
+
+  $fontStyleMap = [
+    'font-style--normal' => 'normal',
+    'font-style--italic' => 'italic',
+  ];
+
+$customTheme = $typeTheme->custom_theme();
 
 
 ?>
@@ -47,9 +63,13 @@ $renderCssClass = isset($renderCssClass) ? $renderCssClass : true;
   {
 <?php endif?>
     
-<?php if($fontSize): ?>
-  --fontsize-factor: <?= $fontSizesMap[$fontSize] ?>;
-<?php endif?>
+  <?php if($fontSize): ?>--fontsize-factor: <?= $fontSizesMap[$fontSize] ?>;<?php endif?>
+
+  <?php if($fontWeight): ?>font-weight: <?= $fontWeightMap[$fontWeight] ?>;<?php endif?>
+
+  <?php if($fontWeight): ?>font-style: <?= $fontStyleMap[$fontStyle] ?>;<?php endif?>
+  
+  <?php if($customTheme->isNotEmpty()): ?><?= $customTheme ?><?php endif?>
 
 <?php if($renderCssClass): ?>
 }
