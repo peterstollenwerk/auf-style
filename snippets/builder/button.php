274defaa8320e9795c9d56892ed9c4e350dfff45
PeterStollenwerk <ps@peterstollenwerk.de>
2020-08-27 21:22:16 +0200

    done

--- snippets/builder/button.php
+++ snippets/builder/button.php
@@ -1,6 +1,6 @@
 <?php
   $text = $data->text()->kirbytextinline();
   $url = $data->url();
-  $class = 'button box box-theme--'. $data->box_theme();
+  $cssClasses = $data->style_settings()->toCssClasses();
 ?>
-<a class="<?= $class ?>" href="<?= $url ?>"><?= $text ?></a>
+<a class="<?= $cssClasses ?>" href="<?= $url ?>"><?= $text ?></a>
--- snippets/builder/editor.php
+++ snippets/builder/editor.php
@@ -7,37 +7,13 @@
   $type = $data->type();
   $typeClass = ($type->isNotEmpty()) ? $type : '';
   
-  $boxTheme = $data->box_theme();
-
-  $boxThemeClass = Style::getBoxThemeClassByBoxTheme($boxTheme);
-  
-  $typeTheme = $data->type_theme();
-  $typeThemeClass = ($typeTheme->isNotEmpty()) ? 'type-theme--'. $typeTheme : '';
-  
-  $fontSize = $data->font_size();
-  $fontSizeClass = ($fontSize->isNotEmpty()) ? $fontSize : '';
-  
-  $fontWeight = $data->font_weight();
-  $fontWeightClass = ($fontWeight->isNotEmpty()) ? $fontWeight : '';
-  
-  $fontStyle = $data->font_style();
-  $fontStyleClass = ($fontStyle->isNotEmpty()) ? $fontStyle : '';
-
-  $cssClasses = [];
-  array_push($cssClasses, $typeClass);
-  array_push($cssClasses, $boxThemeClass);
-  array_push($cssClasses, $typeThemeClass);
-  array_push($cssClasses, $fontSizeClass);
-  array_push($cssClasses, $fontWeightClass);
-  array_push($cssClasses, $fontStyleClass);
-
-  $cssClass = implode(' ', array_filter($cssClasses));
+  $cssClasses = $data->style_settings()->toCssClasses();
 ?>
 
 
-<div class="<?= $cssClass ?>">
+<div class="<?= $cssClasses ?>">
 <?php foreach($blocks as $block): ?>
-  <?php $block->attrs()->update(['class' => $cssClass]); ?>
+  <?php $block->attrs()->update(['class' => $cssClasses]); ?>
     <?= $block ?>
   <?php endforeach?>
 </div>
