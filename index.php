fe76a38da3c02d97d469467a6d7ad205dfa31530
PeterStollenwerk <ps@peterstollenwerk.de>
2020-08-31 16:20:32 +0200

    corrected »site/plugins/auf-style folder-name for correct composer export

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 # Instead of loading the classes via the load helper
 # set them in the composer json
 # to make the classes work with php unit!
+@include_once __DIR__ . '/vendor/autoload.php'; # all classes set in composer.json > psr-4 are loaded here!
 
 use auf\Style;
 use Kirby\Cms\Field;
 use Kirby\Cms\Page;
 
-@include_once __DIR__ . '/vendor/autoload.php'; # all classes set in composer.json > psr-4 are loaded here!
 
 Kirby::plugin('auf/style', [
     'fieldMethods' => [
