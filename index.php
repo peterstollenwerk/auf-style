037087658ba035864fc2359d5d57707f23f96f0b
PeterStollenwerk <ps@peterstollenwerk.de>
2020-08-31 20:20:05 +0200

    changed namespace

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
 <?php
 
-# Instead of loading the classes via the load helper
-# set them in the composer json
-# to make the classes work with php unit!
 @include_once __DIR__ . '/vendor/autoload.php'; # all classes set in composer.json > psr-4 are loaded here!
 
-use auf_style\Style;
+use auf\Style;
 use Kirby\Cms\Field;
 use Kirby\Cms\Page;
 
--- lib/Style.php
+++ lib/Style.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace auf_style;
+namespace auf;
 
 use Kirby\Cms\Field;
 
